77b3348dff74ead46469a5c8bdec407dd8ef257d
DisciplinedPerson <tookitseriousaboutlife@gmail.com>
2026-01-09 17:43:33 +0200

    start if config

---
+++ lua/user/config/options.lua
@@ -0,0 +1,25 @@
+vim.g.netrw_liststyle = 3 
+
+local opts = vim.opt
+
+opts.number = true
+opts.relativenumber = true
+
+opts.wrap = false
+opts.autoindent = true
+opts.smartindent = true
+opts.ignorecase = true
+
+opts.splitright = true
+opts.splitbelow = true
+
+opts.shiftwidth = 4
+opts.tabstop = 4
+
+opts.termguicolors = true
+
+opts.clipboard = "unnamed"
+
+opts.scrolloff = 8
+
+opts.guicursor = ""
